115582c876efa3aa2131dec40dd0fd59824d37a8
Vasiliy Yagunov <vasiliy.yagunov@mail.ru>
2020-08-29 12:31:59 +0300

    model js

---
+++ src/model.js
@@ -0,0 +1,10 @@
+export const model = [
+    {type: 'title', value: 'Test title', options: {}},
+    {type: 'text', value: 'Lorem ipsum dolor sit amet consectetur', options: {}},
+    {type: 'textColums', value: [
+        '1 text',
+        '2 text',
+        '3 text',
+        '4 text'
+    ], options: {}}
+]
